7c6dd0079a9d9f5cc92415719da55fdb997ccfd6
jer-hub <jerwin.carreon@jmc.edu.ph>
2025-06-26 21:51:03 +0800

    updated

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Creating virtual environment..."
-python3.13 -m venv python3-virtualenv
+python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 
 echo "Installing dependencies..."
